340db6f005c031dff117c4deec493b76548134e4
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-08-26 06:26:17 +0530

    expense shell

---
+++ backend.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute the script"
+        exit 1 #other than 0
+    fi
+}
+
+echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Disabling existing default NodeJS"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling NodeJS 20"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing NodeJS"
+
+useradd expense &>>$LOG_FILE_NAME
+VALIDATE $? "Adding expense user"
+
+mkdir /app
+VALIDATE $? "Creating app directory"
+
+cd /app
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading Backend"
+
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Unzipping Backend"
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "Installing dependencies"
+
+cp home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+# Prepare MySQL Schema
+
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h mysql.svrk.fun -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Setting up the transactions and tables"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "Daemon Reload"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling backend"
+
+systemctl start backend &>>$LOG_FILE_NAME
+VALIDATE $? "Starting backend"
