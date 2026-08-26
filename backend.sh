077d5424583c2567f36cfde24d4840c977b7f251
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-08-26 07:03:29 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -49,7 +49,7 @@ then
     useradd expense &>>$LOG_FILE_NAME
     VALIDATE $? "Adding expense user"
 else
-    echo "expense user already exists ... $Y SKIPPING $N"
+    echo -e "expense user already exists ... $Y SKIPPING $N"
 fi
 
 mkdir -p /app &>>$LOG_FILE_NAME
